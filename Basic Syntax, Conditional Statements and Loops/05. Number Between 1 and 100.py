e0a889b7461cd4af6eb45d472b462689b0ddc59e
Dimitar Dimitrov <112943652+MitkoVtori@users.noreply.github.com>
2022-09-22 20:43:07 +0300

    Delete Basic Syntax, Conditional Statements and Loops directory

--- Basic Syntax, Conditional Statements and Loops/05. Number Between 1 and 100.py
+++
@@ -1,5 +0,0 @@
-num = float(input())
-while num < 1 or num > 100:
-    num = float(input())
-
-print(f"The number {num} is between 1 and 100")
--- Basic Syntax, Conditional Statements and Loops/07. Patterns.py
+++
@@ -1,7 +0,0 @@
-num = int(input())
-
-for i in range(1, num + 1):
-    print(i * '*')
-
-for n in range(num - 1, 0, - 1):
-    print(n * '*')
