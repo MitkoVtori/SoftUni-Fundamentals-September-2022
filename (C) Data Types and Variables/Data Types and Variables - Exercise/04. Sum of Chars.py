def9a733f1d976502bad6c786bee4708bdc82ec6
Dimitar Dimitrov <112943652+MitkoVtori@users.noreply.github.com>
2022-09-24 20:43:03 +0300

    Update 04. Sum of Chars.py

--- (C) Data Types and Variables/Data Types and Variables - Exercise/04. Sum of Chars.py
+++ (C) Data Types and Variables/Data Types and Variables - Exercise/04. Sum of Chars.py
@@ -1,7 +1,7 @@
-n = int(input())
+lines = int(input())
 total_sum = 0
 
-for i in range(n):
+for chars in range(lines):
     letter = input()
     total_sum += ord(letter)
 print(f"The sum equals: {total_sum}")
