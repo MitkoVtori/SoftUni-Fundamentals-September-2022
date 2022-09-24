b2b3a5a817ef53d9aece332a20466beb783e958c
Dimitar Dimitrov <112943652+MitkoVtori@users.noreply.github.com>
2022-09-24 20:51:01 +0300

    Update 06. Next Happy Year.py

--- (C) Data Types and Variables/Data Types and Variables - Lab/06. Next Happy Year.py
+++ (C) Data Types and Variables/Data Types and Variables - Lab/06. Next Happy Year.py
@@ -5,7 +5,7 @@ while not happy_year:
     year += 1
     set_year = set()
 
-    for i in range(len(str(year))):
+    for years in range(len(str(year))):
         set_year.add(str(year)[i])
 
         happy_year = len(set_year) == len(str(year))
