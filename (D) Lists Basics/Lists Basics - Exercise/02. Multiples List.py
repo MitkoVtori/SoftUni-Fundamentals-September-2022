9ef857e5414bbcde5fbb2c57909ba4737af36db8
Dimitar Dimitrov <112943652+MitkoVtori@users.noreply.github.com>
2022-09-24 19:53:18 +0300

    Update 02. Multiples List.py

--- (D) Lists Basics/Lists Basics - Exercise/02. Multiples List.py
+++ (D) Lists Basics/Lists Basics - Exercise/02. Multiples List.py
@@ -1,11 +1,12 @@
-first_num = int(input())
-first_cnt = first_num
-second_num = int(input())
-second_cnt = second_num
+first_number = int(input())
+first_number_cnt = first_number
+second_number = int(input())
 list_numbers = []
 
-for multiply in range(1, second_num + 1):
-    list_numbers.append(first_cnt)
-    first_cnt += first_num
+for multiply in range(1, second_number + 1):
+    list_numbers.append(first_number_cnt)
+    # We use the first_number_cnt because for example, if first_number = 2, 
+    # and we add first_number to first_number it will become 4, and after that instead of 4 + 2 we'll have 4 + 4 and that gives us wrong information.
+    first_number_cnt += first_number
 
 print(list_numbers)
