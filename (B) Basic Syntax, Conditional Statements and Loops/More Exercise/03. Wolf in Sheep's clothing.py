513e1ea64f20c046bbac309270453664c0aa66ff
Dimitar Dimitrov <112943652+MitkoVtori@users.noreply.github.com>
2022-09-24 20:38:34 +0300

    Update 03. Wolf in Sheep's clothing.py

--- (B) Basic Syntax, Conditional Statements and Loops/More Exercise/03. Wolf in Sheep's clothing.py
+++ (B) Basic Syntax, Conditional Statements and Loops/More Exercise/03. Wolf in Sheep's clothing.py
@@ -5,8 +5,8 @@ if animal[-1] == "wolf":
 else:
     if "wolf" in animal:
         animal.reverse()
-            for index, value in enumerate(animal):
-                if value == "wolf":
-                    print(f"Oi! Sheep number {index}! You are about to be eaten by a wolf!")
-     else:
-         pass
+        for index, value in enumerate(animal):
+            if value == "wolf":
+                print(f"Oi! Sheep number {index}! You are about to be eaten by a wolf!")
+    else:
+        pass
