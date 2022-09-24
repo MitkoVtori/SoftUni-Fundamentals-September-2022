f68fbca31f9b24172b419eff850a0d44ccc7d014
Dimitar Dimitrov <112943652+MitkoVtori@users.noreply.github.com>
2022-09-24 19:39:44 +0300

    Update 05. Numbers Filter.py

--- (D) Lists Basics/Lists Basics - Lab/05. Numbers Filter.py
+++ (D) Lists Basics/Lists Basics - Lab/05. Numbers Filter.py
@@ -1,26 +1,26 @@
 n = int(input())
-COMMAND_EVEN = "even"
-COMMAND_ODD = "odd"
-COMMAND_POSITIVE = "positive"
-COMMAND_NEGATIVE = "negative"
+command_even = "even"
+command_odd = "odd"
+command_positive = "positive"
+command_negative = "negative"
 my_list = []
 filtered_numbers = []
 
-for i in range(n):
+for num in range(n):
     curr_num = int(input())
     my_list.append(curr_num)
 
 command = input()
 
-for num in my_list:
+for number in my_list:
     filtered_command = (
-        (command == COMMAND_EVEN and num % 2 == 0) or
-        (command == COMMAND_ODD and num % 2 != 0) or
-        (command == COMMAND_POSITIVE and num >= 0) or
-        (command == COMMAND_NEGATIVE and num < 0)
+        (command == command_even and number % 2 == 0) or
+        (command == command_odd and number % 2 != 0) or
+        (command == command_positive and number >= 0) or
+        (command == command_negative and number < 0)
     )
 
     if filtered_command:
-        filtered_numbers.append(num)
+        filtered_numbers.append(number)
 
 print(filtered_numbers)
