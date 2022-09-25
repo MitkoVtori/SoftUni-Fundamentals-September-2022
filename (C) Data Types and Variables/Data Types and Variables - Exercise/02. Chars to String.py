0d8c6c83b0c7ac2a06852888f94c8a155dbe993c
Dimitar Dimitrov <112943652+MitkoVtori@users.noreply.github.com>
2022-09-25 09:05:11 +0300

    Update 02. Chars to String.py

--- (C) Data Types and Variables/Data Types and Variables - Exercise/02. Chars to String.py
+++ (C) Data Types and Variables/Data Types and Variables - Exercise/02. Chars to String.py
@@ -1,5 +1,7 @@
-first_character = input()
-second_character = input()
-third_character = input()
-  
-print(f"{first_character}{second_character}{third_character}")
+string = ''
+
+for char in range(3):
+    character = input()
+    string += character
+
+print(string)
