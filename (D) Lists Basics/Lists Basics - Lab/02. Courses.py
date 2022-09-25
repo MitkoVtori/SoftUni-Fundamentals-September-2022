8bdf80649b8a55f620f2b81b8f3cc382072d82f6
Dimitar Dimitrov <112943652+MitkoVtori@users.noreply.github.com>
2022-09-25 10:00:42 +0300

    Update 02. Courses.py

--- (D) Lists Basics/Lists Basics - Lab/02. Courses.py
+++ (D) Lists Basics/Lists Basics - Lab/02. Courses.py
@@ -1,8 +1,8 @@
-n = int(input())
+number_courses = int(input())
 # EMPTY LIST:
 courses = list() # or []
 
-for course in range(n):
+for course in range(number_courses):
     curr_course = input()
     courses.append(curr_course) # adding the course to the list
 print(courses)
