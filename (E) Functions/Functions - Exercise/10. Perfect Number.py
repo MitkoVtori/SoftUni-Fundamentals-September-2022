59a945a12077eaa6a6538c89274a74a33a0f2a1c
Dimitar Dimitrov <112943652+MitkoVtori@users.noreply.github.com>
2022-10-07 20:00:01 +0300

    Update 10. Perfect Number.py

--- (E) Functions/Functions - Exercise/10. Perfect Number.py
+++ (E) Functions/Functions - Exercise/10. Perfect Number.py
@@ -1,11 +1,14 @@
+def perfect_num(num, divisor):
+    for divide in range(1, num):
+        if num % divide == 0:
+            divisor += divide
+
+    if divisor == num:
+        return "We have a perfect number!"
+    else:
+        return "It's not so perfect."
+
+    
 number = int(input())
 divisors = 0
-
-for divide in range(1, number):
-    if number % divide == 0:
-        divisors += divide
-
-if divisors == number:
-    print("We have a perfect number!")
-else:
-    print("It's not so perfect.")
+print(perfect_num(number, divisors))
