f0d91474a3c9e2236514d9b2acc97243a3c97b88
Dimitar Dimitrov <112943652+MitkoVtori@users.noreply.github.com>
2022-10-07 19:37:41 +0300

    Update 04. Odd and Even Sum.py

--- (E) Functions/Functions - Exercise/04. Odd and Even Sum.py
+++ (E) Functions/Functions - Exercise/04. Odd and Even Sum.py
@@ -1,12 +1,15 @@
+def even_odd_sum(numbers, even, odd):
+    for index in range(len(numbers)):
+        current_number = int(numbers[index])
+        if current_number % 2 == 0:
+            even += current_number
+        else:
+            odd += current_number
+
+    return f'Odd sum = {odd}, Even sum = {even}'
+
+
 numbers = input()
-odd_sum = 0
 even_sum = 0
-
-for index in range(len(numbers)):
-    current_number = int(numbers[index])
-    if current_number % 2 == 0:
-        even_sum += current_number
-    else:
-        odd_sum += current_number
-
-print(f'Odd sum = {odd_sum}, Even sum = {even_sum}')
+odd_sum = 0
+print(even_odd_sum(numbers, even_sum, odd_sum))
