f6db90d762d46a662db3a971d54487fef31f0719
Dimitar Dimitrov <112943652+MitkoVtori@users.noreply.github.com>
2022-10-07 19:41:59 +0300

    Update 08. Palindrome Integers.py

--- (E) Functions/Functions - Exercise/08. Palindrome Integers.py
+++ (E) Functions/Functions - Exercise/08. Palindrome Integers.py
@@ -1,8 +1,12 @@
-numbers = input().split(', ')
+def palindrome_integers(numbers):
 
-for palindrome in range(len(numbers)):
-    current_number = numbers[palindrome][::-1]
-    if current_number == numbers[palindrome]:
-        print("True")
-    else:
-        print("False")
+    for palindrome in range(len(numbers)):
+        current_number = numbers[palindrome][::-1]
+        if current_number == numbers[palindrome]:
+            print("True")
+        else:
+            print("False")
+            
+            
+list_numbers = input().split(', ')
+palindrome_integers(list_numbers)
