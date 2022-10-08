42c5bec3bad30a10bb78d409cce3f65d08b16d2f
Dimitar Dimitrov <112943652+MitkoVtori@users.noreply.github.com>
2022-10-08 10:27:51 +0300

    Update 02. Add and Subtract.py

--- (E) Functions/Functions - Exercise/02. Add and Subtract.py
+++ (E) Functions/Functions - Exercise/02. Add and Subtract.py
@@ -1,5 +1,15 @@
-def add_and_subtract(a, b, c):
-    return (a + b) - c
+def sum_numbers(first, second):
+    return first + second
+
+
+def subtract(sum_first_second, third):
+    return sum_first_second - third
+
+
+def add_and_subtract(first, second, third):
+    sum_of_first_and_second = sum_numbers(first, second)
+    result = subtract(sum_of_first_and_second, third)
+    return result
 
 
 first_number = int(input())
