f246181b353cafbd9382cadf06b1f2aeca111914
Dimitar Dimitrov <112943652+MitkoVtori@users.noreply.github.com>
2022-10-08 16:32:49 +0300

    Update 11. Loading Bar.py

--- (A) Not Included In Final Score - Exercise/(4) Functions/11. Loading Bar.py
+++ (A) Not Included In Final Score - Exercise/(4) Functions/11. Loading Bar.py
@@ -1,25 +1,8 @@
-def loading_bar_percentage(bar_percentage, loading_bar):
-    for percent in range(int(bar_percentage[0])):
-        if int(bar_percentage) >= 10:
-            loading_bar.append('%')
+def loading_bar(percent):
+    if percent == 100:
+        return f"100% Complete!\n[{'%' * 10}]"
+    return f"{percent}% [{'%' * (percent // 10)}{'.' * (10 - percent // 10)}]\nStill loading..."
 
-    for dots in range(10 - int(bar_percentage[0])):
-        if int(bar_percentage) >= 10:
-            loading_bar.append('.')
 
-    if int(bar_percentage) < 10:
-        loading_bar.append('.' * 10)
-
-    loading_bar = ''.join(loading_bar)
-
-    if bar_percentage == '100':
-        print("100% Complete!")
-        print(f"[{'%' * 10}]")
-    else:
-        print(f"{bar_percentage}% [{loading_bar}]")
-        print("Still loading...")
-        
-        
-bar = input()
-loading = []
-loading_bar_percentage(bar, loading)
+loading_percent = int(input())
+print(loading_bar(loading_percent))
