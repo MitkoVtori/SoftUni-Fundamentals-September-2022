330c4d11d5d8ef661c25b96bb78a1943a92256bb
Dimitar Dimitrov <112943652+MitkoVtori@users.noreply.github.com>
2022-10-10 09:43:08 +0300

    Update 02. Shoot for the Win.py

--- (G) Middle Exam Preparation/03. Programming Fundamentals Mid Exam Retake/02. Shoot for the Win.py
+++ (G) Middle Exam Preparation/03. Programming Fundamentals Mid Exam Retake/02. Shoot for the Win.py
@@ -1,20 +1,28 @@
-array = list(map(int, input().split(' ')))
-
+targets = list(map(int, input().split(' ')))
+targets_counter = 0
 while True:
     command = input()
-    if command == 'end':
+
+    if command == 'End':
         break
 
-    if command == 'decrease':
-        for element in range(len(array)):
-            array[element] -= 1
+    current_target = int(command)
+    if current_target >= len(targets):
         continue
-    indexes = command.split(' ')
-    index1 = int(indexes[1])
-    index2 = int(indexes[2])
-    if indexes[0] == 'swap':
-        array[index1], array[index2] = array[index2], array[index1]
-    if indexes[0] == 'multiply':
-        array[index1] *= array[index2]
+    if targets[current_target] == -1:
+        continue
 
-print(*array, sep=', ')
+    for index in range(len(targets)):
+        if targets[index] == -1:
+            continue
+        if index == current_target:
+            continue
+        elif targets[index] > targets[current_target]:
+            targets[index] -= targets[current_target]
+        else:
+            targets[index] += targets[current_target]
+
+    targets[current_target] = -1
+    targets_counter += 1
+
+print(f"Shot targets: {targets_counter} ->", *targets, sep=' ')
