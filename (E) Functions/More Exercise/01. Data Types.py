b3d1fdb19841a7ebdc8f7b3a39ffd45c9d2e2a54
Dimitar Dimitrov <112943652+MitkoVtori@users.noreply.github.com>
2022-10-11 17:28:10 +0300

    Update 01. Data Types.py

--- (E) Functions/More Exercise/01. Data Types.py
+++ (E) Functions/More Exercise/01. Data Types.py
@@ -1,9 +1,15 @@
+def data_types(data_type, value):
+    result = 0
+    if data_type == "int":
+        result = int(value) * 2
+    elif data_type == "real":
+        result = f"{float(value) * 1.5:.2f}"
+    else:
+        result = f"${value}$"
+    
+    return result
+
+
 type_command = input()
 command = input()
-
-if type_command == 'int':
-    print(int(command) * 2)
-elif type_command == "real":
-    print(f"{float(command) * 1.5:.2f}")
-else:
-    print(f"${command}$")
+print(data_types(type_command, command))
