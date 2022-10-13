ddf52d9c0b13f34eb9462d7356906942d073bfc7
Dimitar Dimitrov <112943652+MitkoVtori@users.noreply.github.com>
2022-10-13 17:53:50 +0300

    Update 04. Palindrome String.py

--- (F) Lists Advanced/04. Palindrome String.py
+++ (F) Lists Advanced/04. Palindrome String.py
@@ -1,12 +1,9 @@
 words = input().split(' ')
 palindrome = input()
-palindromes_list = []
+palindromes_list = [word for word in words if word == word[::-1]]
 
-for index in range(len(words)):
-    if words[index] == words[index][::-1]:
-        palindromes_list.append(words[index])
-
-palindromes_count = palindromes_list.count(palindrome)
+found_palindromes = [word for word in words if word == palindrome]
+palindrome_counter = len(found_palindromes)
 
 print(palindromes_list)
-print(f"Found palindrome {palindromes_count} times")
+print(f"Found palindrome {palindrome_counter} times")
