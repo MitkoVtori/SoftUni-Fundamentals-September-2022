6ed6ccc27fd044b3a9778fda12fd61f01fe30cac
Dimitar Dimitrov <112943652+MitkoVtori@users.noreply.github.com>
2022-11-19 09:05:22 +0200

    Update 04. Extract Emails.py

--- (L) Regular Expressions/Regular Expressions - Exercise/04. Extract Emails.py
+++ (L) Regular Expressions/Regular Expressions - Exercise/04. Extract Emails.py
@@ -1,6 +1,7 @@
 import re
+ 
 string = input()
-pattern = re.compile(r"(^|(?<=\s))([A-Za-z0-9])+([\.\-\_][A-Za-z0-9]+)*@([A-Za-z-]+)\.([A-Za-z-]+)([\.A-Za-z-])*(\b|(?=\s))")
-emails = re.finditer(pattern, string)
+pattern = r'\s(([a-z0-9]+[a-z0-9\.\-\_]*)@[a-z\-]+(\.[a-z]+)+)\b'
+emails = re.findall(pattern, string)
 for email in emails:
     print(email[0])
