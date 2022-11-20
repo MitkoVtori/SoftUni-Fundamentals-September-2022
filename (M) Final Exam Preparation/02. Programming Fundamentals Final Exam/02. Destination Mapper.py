aa622ddbe8f014f53249455fa20d0c177841131a
Dimitar Dimitrov <112943652+MitkoVtori@users.noreply.github.com>
2022-11-20 12:04:24 +0200

    Update 02. Destination Mapper.py

--- (M) Final Exam Preparation/02. Programming Fundamentals Final Exam/02. Destination Mapper.py
+++ (M) Final Exam Preparation/02. Programming Fundamentals Final Exam/02. Destination Mapper.py
@@ -3,8 +3,10 @@ import re
 pattern = r"([\/=])([A-Z][a-zA-z]{2,})\1"
 places = input()
 valid_places = re.findall(pattern, places)
+
 travel_points = 0
 destinations = []
+
 for current_place in valid_places:
     travel_points += len(current_place[1])
     destinations.append(current_place[1])
